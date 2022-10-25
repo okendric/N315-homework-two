68fb426508156ebf10a89511124aff72f6009c73
okendric <73384318+okendric@users.noreply.github.com>
2022-10-25 04:11:00 -0400

    Add files via upload

--- app/app.js
+++ app/app.js
@@ -1,3 +1,22 @@
+function changeRoute(){
+    let hashTag = window.location.hash;
+    let pageID = hashTag.replace("#", "");
+    //console.log(hashTag + " " + pageID);
+
+    if (pageID != "") {
+       $.get(`pages/${pageID}/${pageID}.html`, function(data){
+        //console.log("data " + data);
+        $("#app").html(data);
+        });
+    } else {
+        $.get(`pages/home/home.html`, function(data){
+            //console.log("data " + data);
+            $("#app").html(data);
+            });
+    }
+    
+}
+
 function initListeners() {
     $(".bars").click(function(e) {
         $(".bars").toggleClass("active");
@@ -8,6 +27,9 @@ function initListeners() {
         $(".bars").removeClass("active");
         $(".links").removeClass("active");
     })
+
+    $(window).on("hashchange", changeRoute);
+    changeRoute();
 }
 
 
